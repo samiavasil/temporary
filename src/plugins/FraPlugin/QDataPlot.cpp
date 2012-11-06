db2458d3f60a516b6c042eddbed1efeb73aa9435
vasil <vasil@dce.bg>
2012-11-06 19:35:22 +0200

    Add picker base implementation

--- src/plugins/FraPlugin/QDataPlot.cpp
+++ src/plugins/FraPlugin/QDataPlot.cpp
@@ -3,26 +3,120 @@
 #include<QBoxLayout>
 #include<QToolBar>
 #include<QMenuBar>
-#include<qwt/qwt_plot_curve.h>
 #include<qwt/qwt_plot_canvas.h>
 #include<qwt/qwt_plot_grid.h>
 #include<qwt/qwt_plot_zoomer.h>
 #include <qwt/qwt_painter.h>
 #include <qwt/qwt_plot_layout.h>
+#include <qwt/qwt_symbol.h>
+
+
+#include <QMouseEvent>
+/*
+class  QwtMyPicker:public QwtPlotPicker{
+public:
+    explicit QwtMyPicker( QwtPlotCanvas * );
+    virtual ~QwtMyPicker();
+
+protected:
+    QwtText QwtPlotPicker::trackerText( const QPoint &pos ) const
+    {
+        QwtScaleMap xMap = plot()->canvasMap( d_xAxis );
+        QwtScaleMap yMap = plot()->canvasMap( d_yAxis );
+
+        return trackerTextF( invTransform( pos ) );
+    }
+};*/
+
+bool QDataPlot::MouseEvEater::eventFilter(QObject *obj, QEvent *event)
+{
+      //return QObject::eventFilter(obj, event);
+
+    if ( event->type() == QEvent::MouseMove ) {
+        QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+        qDebug("Ate key press %d", mouseEvent->y());
+
+        if( m_Plot ){
+            if( ( 0 <= m_Plot->m_CurCurve )&&( m_Plot->m_CurCurve < m_Plot->m_Curves.count() ) ){
+                QwtPlotCurve* curCurve = m_Plot->m_Curves.value(m_Plot->m_CurCurve,NULL);
+                if( curCurve ){
+                    //int idx = curCurve->closestPoint( mouseEvent->pos() );
+                    int size = curCurve->dataSize();
+                    QPointF data;// = curCurve->sample(idx);
+                    const QwtScaleMap yMap = curCurve->plot()->canvasMap( curCurve->yAxis() );
+                    const QwtScaleMap xMap = curCurve->plot()->canvasMap( curCurve->xAxis() );
+                    QPointF point = m_Plot->m_picker->trackerPosition();
+                    qDebug("Tracker postion %f %f ", point.x(), point.y());
+                    double x = xMap.invTransform( point.x() );
+                    double y = yMap.invTransform( point.y() );
+                    QwtInterval interval = curCurve->plot()->axisInterval(QwtPlot::xBottom);
+                    double xx = interval.minValue();
+                    double ww = xx + interval.width();
+                    qDebug("Coordinate X = %f Y = %f ", x , y);
+                    qDebug("Coordinate XX = %f WW = %f ", xx , ww);
+                    for( int i=0; i < size; i++ ){
+                        data = curCurve->sample(i);
+                    //    qDebug("Data X = %f Y = %f ", data.x() , data.y());
+                        if( (xx < data.x())&&( ww > data.x() ) ){
+                            if( data.x() >= x ){
+                                // if( i > 0 )
+                               //  data = curCurve->sample(i);
+                                break;
+                            }
+                        }
+                    }
+
+                    x = xMap.transform( data.x() );
+                    y = yMap.transform( data.y() );
+                     /*int idx = curCurve->closestPoint( QPoint(x,y) );
+                    data = curCurve->sample(idx);
+                    x = xMap.transform( data.x() );
+                    y = yMap.transform( data.y() );*/
+                    ((QPoint*)(&(mouseEvent->pos())))->setY(x);
+                    ((QPoint*)(&(mouseEvent->pos())))->setY(y);
+                }
+            }
+            //
+        }
+        // standard event processing
+        return QObject::eventFilter(obj, mouseEvent);
+    }
+    /*else{
+        // standard event processing
+         return QObject::eventFilter(obj, event);
+     }*/
+}
+
 
 QDataPlot::QDataPlot(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::QDataPlot)
 {
+    m_CurCurve = -1;
     // add curves
     QwtPlotCurve *curve1 = new QwtPlotCurve("Curve 1");
     QwtPlotCurve *curve2 = new QwtPlotCurve("Curve 2");
+    curve1->setStyle( QwtPlotCurve::Lines );
+    //curve2->setStyle( QwtPlotCurve::NoCurve );
+    QwtSymbol* sym = new QwtSymbol(QwtSymbol::Rect,QBrush(Qt::blue),QPen( QBrush(Qt::blue),1 ),QSize(6,6) );
+    curve2->setSymbol(sym);
     double x[100],y[100],z[100];
 
     ui->setupUi(this);
 
+    // ui->PlotQwt->setAxisScale( );
+
+    //ui->PlotQwt->setAxisAutoScale(QwtPlot::yLeft);
+    //ui->PlotQwt->setAxisAutoScale(QwtPlot::yRight);
+    //ui->PlotQwt->setAxisAutoScale(QwtPlot::xBottom);
 
 
+    ui->PlotQwt->canvas()->setPaintAttribute( QwtPlotCanvas::BackingStore, false );
+    ui->PlotQwt->canvas()->setFrameStyle ( QFrame::Box | QFrame::Plain );
+    ui->PlotQwt->canvas()->setLineWidth ( 1 );
+    ui->PlotQwt->setAutoReplot ( false );
+
+    ui->PlotQwt->enableAxis ( QwtPlot::yRight, true );
 
     /* */
     QMenuBar* ptr1 = new QMenuBar();
@@ -50,40 +144,14 @@ QDataPlot::QDataPlot(QWidget *parent) :
 
     ptr->addWidget(ui->ComboLineColor);
 
+    //ui->PlotQwt->setAxisAutoScale(QwtPlot::yRight);
 
-
-
-
-
-
-    ui->PlotQwt->canvas()->setPaintAttribute( QwtPlotCanvas::BackingStore, false );
-    ui->PlotQwt->canvas()->setFrameStyle ( QFrame::Box | QFrame::Plain );
-    ui->PlotQwt->canvas()->setLineWidth ( 1 );
-
-    //ui->PlotQwt->setMargin ( 5 );
-    ui->PlotQwt->enableAxis ( QwtPlot::yRight, true );
-    ui->PlotQwt->setAutoReplot ( false );
-    ui->PlotQwt->plotLayout()->setAlignCanvasToScales( true );
-
-    ui->PlotQwt->setAxisAutoScale(QwtPlot::yLeft);
-    ui->PlotQwt->setAxisAutoScale(QwtPlot::yRight);
-    ui->PlotQwt->setAxisAutoScale(QwtPlot::xBottom);
-
-    // We don't need the cache here
-    //ui->PlotQwt->canvas()->setPaintAttribute ( QwtPlotCanvas::PaintCached, false );
-    //ui->PlotQwt->canvas()->setPaintAttribute ( QwtPlotCanvas::PaintPacked, true );
-    //alignScales();
 
     // Assign a title to plot
     ui->PlotQwt->setTitle ( "Impedance" );
     //ui->PlotQwt->insertLegend ( new QwtLegend(), QwtPlot::BottomLegend );
     ui->PlotQwt->setCanvasBackground ( QColor ( Qt::black ) );
     ui->PlotTable->setVisible( false );
-
-
-
-
-
 
     m_Grid = new QwtPlotGrid;
     m_Grid->enableXMin ( true );
@@ -93,55 +161,87 @@ QDataPlot::QDataPlot(QWidget *parent) :
     m_Grid->enableY ( false );
     m_Grid->attach ( ui->PlotQwt );
     m_Grid->setPen ( QPen ( Qt::white ) );
+    m_Grid->setYAxis(QwtPlot::yRight);
+
+    m_Zoomer[0] = new QwtPlotZoomer(QwtPlot::xBottom, QwtPlot::yRight,ui->PlotQwt->canvas());
+    m_Zoomer[0]->setTrackerMode(QwtPicker::AlwaysOff);
+    m_Zoomer[0]->setRubberBand(QwtPicker::RectRubberBand);
+    m_Zoomer[0]->setRubberBandPen(QColor ( Qt::green ) );
+    m_Zoomer[0]->setTrackerMode(QwtPicker::ActiveOnly);
+    m_Zoomer[0]->setTrackerPen( QColor ( Qt::green ) );
+    m_Zoomer[0]->setEnabled( false );
+
+    m_Zoomer[0]->setMousePattern ( QwtEventPattern::MouseSelect2, Qt::RightButton, Qt::ControlModifier );
+    m_Zoomer[0]->setMousePattern ( QwtEventPattern::MouseSelect3, Qt::RightButton );
+
+     //m_Zoomer[0]->setAxis();
+
+   m_Zoomer[1] = new QwtPlotZoomer( QwtPlot::xTop, QwtPlot::yLeft, ui->PlotQwt->canvas() );
+   m_Zoomer[1]->setEnabled( false );
 
 
+    m_picker = new QwtPlotPicker ( QwtPlot::xBottom, QwtPlot::yRight,
+                                                  QwtPlotPicker::CrossRubberBand,   QwtPicker::AlwaysOn,
+                                                  ui->PlotQwt->canvas() );
+    m_picker->setMousePattern ( QwtEventPattern::MouseSelect1, Qt::LeftButton );
+    m_picker->setRubberBand ( QwtPicker::CrossRubberBand );
+    m_picker->setStateMachine(new QwtPickerTrackerMachine());
+    m_picker->setRubberBandPen ( QColor ( Qt::green ) );
+    m_picker->setTrackerPen ( QColor ( Qt::red ) );
+    m_picker->setResizeMode(  QwtPicker::KeepSize );
+//m_picker->setEnabled(false);
+    MouseEvEater* e = new MouseEvEater( this, m_picker  );
 
+    QWidget *w = m_picker->parentWidget();
+    if ( w )
+    {
+        w->installEventFilter( e );
+    }
+    //ui->PlotQwt->canvas()->installEventFilter(e);
+
+    //m_picker->trackerWidget();
 
     //DELL ME
     // copy the data into the curves
+    m_Curves.append(curve1);
+    m_Curves.append(curve2);
     for( int i=0; i<100; i++){
         x[i] = i;
-        y[i] = i;
-        z[i] = 100*sin((6.28*i)/100);
+        z[i] = -50+i;
+        y[i] = 400*sin((6.28*i)/100);
+        qDebug("sin(%d)=%f",i,z[i]);
     }
+    curve1->setYAxis( QwtPlot::yLeft );
+    curve2->setYAxis( QwtPlot::yRight );
     curve1->attach(ui->PlotQwt);
     curve2->attach(ui->PlotQwt);
 
     curve1->setSamples( x,y,100 );
-    curve2->setSamples( x,z,100 );
+    curve2->setSamples( x,z,100);
 
     curve1->setPen(QPen(Qt::green));
     curve2->setPen(QPen(Qt::red));
-    //;
-    //curve1->data()->boundingRect().height();
-  //  on_actionAutoscale_triggered();
 
-    // finally, refresh the plot
-//    ui->PlotQwt->replot();
-
-   m_Zoomer = new QwtPlotZoomer(QwtPlot::xBottom, QwtPlot::yLeft,ui->PlotQwt->canvas());
-   m_Zoomer->setTrackerMode(QwtPicker::AlwaysOff);
-   //m_Zoomer->setRubberBand(QwtPicker::NoRubberBand);
-   m_Zoomer->setRubberBand(QwtPicker::RectRubberBand);
-   m_Zoomer->setRubberBandPen(QColor ( Qt::green ) );
-   m_Zoomer->setTrackerMode(QwtPicker::ActiveOnly);
-   m_Zoomer->setTrackerPen( QColor ( Qt::green ) );
-
-   m_Zoomer->setMousePattern ( QwtEventPattern::MouseSelect2, Qt::RightButton, Qt::ControlModifier );
-   m_Zoomer->setMousePattern ( QwtEventPattern::MouseSelect3, Qt::RightButton );
+    m_CurCurve = 0;
+    m_Zoomer[0]->setZoomBase( false );
+    ////m_Zoomer[1]->setZoomBase( true );
+    //  ui->PlotQwt->setAxisScale(QwtPlot::yLeft, 0, 100 );
+    //  ui->PlotQwt->setAxisScale(QwtPlot::yRight, -400, 400,1/4  );
 
 
 
-   //  m_Zoomer->initMousePattern(2);
 
-  // m_Zoomer = new QwtPlotZoomer(QwtPlot::xBottom, QwtPlot::yRight,ui->PlotQwt->canvas());
 
-  /* m_Zoomer->setMousePattern ( QwtEventPattern::MouseSelect1,
+    //  m_Zoomer[0]->initMousePattern(2);
+
+    // m_Zoomer[0] = new QwtPlotZoomer(QwtPlot::xBottom, QwtPlot::yRight,ui->PlotQwt->canvas());
+
+    /* m_Zoomer[0]->setMousePattern ( QwtEventPattern::MouseSelect1,
                      Qt::LeftButton );
 
-   m_Zoomer->setMousePattern ( QwtEventPattern::MouseSelect4,
+   m_Zoomer[0]->setMousePattern ( QwtEventPattern::MouseSelect4,
                      Qt::NoButton );
-   m_Zoomer->setMousePattern ( QwtEventPattern::MouseSelect6,
+   m_Zoomer[0]->setMousePattern ( QwtEventPattern::MouseSelect6,
                      Qt::NoButton );  */
 
 }
@@ -154,11 +254,6 @@ QDataPlot::~QDataPlot()
     }
 }
 
-
-void QDataPlot::on_actionMouseTrack_triggered()
-{
-
-}
 
 void QDataPlot::on_actionGrid_X_on_triggered(bool checked)
 {
@@ -188,4 +283,34 @@ void QDataPlot::on_actionAutoscale_triggered()
     ui->PlotQwt->setAxisAutoScale(QwtPlot::yRight);
     ui->PlotQwt->setAxisAutoScale(QwtPlot::xBottom);
     ui->PlotQwt->replot();
+}
+
+void QDataPlot::on_actionMouseTrack_toggled(bool arg1)
+{
+
+    if( arg1  ){
+        m_Zoomer[0]->setTrackerMode(QwtPicker::AlwaysOn);
+    }
+    else{
+        m_Zoomer[0]->setTrackerMode(QwtPicker::ActiveOnly );
+    }
+}
+
+void QDataPlot::on_actionRectangle_Zoom_toggled(bool arg1)
+{
+    if( arg1  ){
+        //m_Zoomer[0]->setEnabled( true );
+        ui->PlotQwt->setEnabled ( arg1 );
+        m_Zoomer[0]->setZoomBase();
+        m_Zoomer[1]->setZoomBase();
+        m_Zoomer[0]->setEnabled ( arg1 );
+        m_Zoomer[0]->zoom ( 0 );
+        m_Zoomer[1]->setEnabled ( arg1 );
+        m_Zoomer[1]->zoom ( 0 );
+    }
+    else{
+        m_Zoomer[0]->setEnabled( false );
+    }
+
+
 }
--- src/plugins/FraPlugin/QDataPlot.h
+++ src/plugins/FraPlugin/QDataPlot.h
@@ -2,11 +2,16 @@
 #define QDATAPLOT_H
 
 #include <QWidget>
+#include <QMap>
 #include<ui_qdataplot.h>
-
-
+#include<qwt/qwt_plot_curve.h>
+#include <qwt/qwt_picker_machine.h>
+#include <qwt/qwt_plot_picker.h>
 class QwtPlotGrid;
 class QwtPlotZoomer;
+
+
+
 class QDataPlot : public QWidget
 {
     Q_OBJECT
@@ -16,7 +21,7 @@ public:
     ~QDataPlot();
     
 private slots:
-    void on_actionMouseTrack_triggered();
+
     void on_actionGrid_X_on_triggered(bool checked);
 
     void on_actionGrid_Y_on_triggered(bool checked);
@@ -25,9 +30,32 @@ private slots:
 
     void on_actionAutoscale_triggered();
 
+    void on_actionMouseTrack_toggled(bool arg1);
+
+    void on_actionRectangle_Zoom_toggled(bool arg1);
+
 protected:
-    QwtPlotGrid*   m_Grid;
-    QwtPlotZoomer* m_Zoomer;
+    class MouseEvEater : public QObject
+    {
+    public:
+        MouseEvEater( QDataPlot* plot , QObject* parent = NULL ):QObject(parent){
+            m_Plot = plot;
+        }
+
+    protected:
+        bool eventFilter(QObject *obj, QEvent *event);
+    protected:
+        QDataPlot* m_Plot;
+    };
+
+    QwtPlotGrid*         m_Grid;
+    QwtPlotZoomer*       m_Zoomer[2];
+
+
+    QMap<QwtPlot::Axis, QwtPlotZoomer*> m_ZoomerMap;
+    QList<QwtPlotCurve*>  m_Curves;
+    QwtPlotPicker*        m_picker;
+    int m_CurCurve;
 private:
     Ui::QDataPlot *ui;
 };
