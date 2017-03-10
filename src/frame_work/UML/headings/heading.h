70fede9d4ffc9a6689b12e26d497a02b77432e93
Vasil Vasilev <samiavasil@gmail.com>
2017-03-10 17:40:53 +0200

    Uml up

---
+++ src/frame_work/UML/headings/heading.h
@@ -0,0 +1,19 @@
+/************************************************************************
+                        %filename% - Copyright %author%
+Daqster software
+Copyright (C) 2016, Vasil Vasilev,  Bulgaria
+
+This file is part of Daqster and its software development toolkit.
+
+Daqster is a free software; you can redistribute it and/or modify it
+under the terms of the GNU Library General Public Licence as published by
+the Free Software Foundation; either version 2 of the Licence, or (at
+your option) any later version.
+
+Daqster is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library
+General Public Licence for more details.
+
+Initial version of this file was generated on %date% at %time%
+**************************************************************************/
